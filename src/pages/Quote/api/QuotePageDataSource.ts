7eaace252dcd1f8ee31e828ef940ad10bfec6fb1
anton62k <anton.kashirov@gmail.com>
2026-05-24 10:32:26 +0700

    Address quote data source review

--- src/pages/Quote/api/QuotePageDataSource.ts
+++ src/pages/Quote/api/QuotePageDataSource.ts
@@ -1,7 +1,9 @@
 import { catalogSnapshot, type CatalogProduct } from 'src/entities/catalog';
 
 export class QuotePageDataSource {
+  private readonly products: readonly CatalogProduct[] = [...catalogSnapshot.products];
+
   getProducts(): readonly CatalogProduct[] {
-    return [...catalogSnapshot.products];
+    return this.products;
   }
 }
